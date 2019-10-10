709e5a33868bb6c79629f39eb73e7a72288f80f0
aleks <alekseevmr@ukr.net>
2019-10-11 01:50:06 +0300

    рабоча версія

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 let bt1=document.getElementById('bt1');
 let bt2=document.getElementById('bt2');
 let used=1;
-
+let registration_button=document.getElementById('registration_button');
 let bt_reg=document.getElementById('registration');
 
 //registration.onclick=function(){
-	$('#registration').on('click', function() { window.location = 'registration.html'; });
+	$('#registration_button').on('click', function() { window.location = 'registration.html'; });
 //}
 
 
@@ -25,7 +25,7 @@ function my_function(){
 	let v=random(0,av.length-1);
 	let znach=av[v];
 	av.splice(v,1);
-	v='images/photo'+znach+'.jpg';
+	v='images/fon'+znach+'.png';
 	document.body.style.backgroundImage='url(\''+v+'\''+')';av.push(used);used=znach;
 	setTimeout(function(){timeout(v)},2000);
 	$("#bt2").fadeToggle({duration:2000});
@@ -34,18 +34,17 @@ setInterval(function(){my_function()},5000);
 
 
 
-document.getElementById('for_leisure').style.fontSize = $(window).height() * 0.05+'px';
-document.getElementById('free').style.fontSize = $(window).height() * 0.05+'px';
-document.getElementById('enter').style.fontSize = $(window).height() * 0.05+'px';
-document.getElementById('registr').style.fontSize = $(window).height() * 0.05+'px';
+
+//document.getElementById('enter').style.fontSize = $(window).height() * 0.05+'px';
+//document.getElementById('registr').style.fontSize = $(window).height() * 0.05+'px';
 
 window.onresize = function() {
 	var w = $(window).width();
 	var h = $(window).height();
-document.getElementById('for_leisure').style.fontSize = h * 0.05+'px';
-	document.getElementById('free').style.fontSize = h * 0.05+'px';
-	document.getElementById('enter').style.fontSize = $(window).height() * 0.05+'px';
-	document.getElementById('registr').style.fontSize = $(window).height() * 0.05+'px';
+//document.getElementById('for_leisure').style.fontSize = h * 0.05+'px';
+	//document.getElementById('free').style.fontSize = h * 0.05+'px';
+	//document.getElementById('enter').style.fontSize = $(window).height() * 0.05+'px';
+	//document.getElementById('registr').style.fontSize = $(window).height() * 0.05+'px';
 }
 
 

d2df94dddca1d1dd1f186d8a75a3bb96e04792bb
aleks <alekseevmr@ukr.net>
2019-10-11 00:53:13 +0300

    слайдер

--- js/index.js
+++ js/index.js
@@ -16,22 +16,28 @@ function random(min, max) {
  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function timeout(){
-	let v=random(0,av.length-1);
-	let znach=av[v];
-	av.splice(v,1);
-	v='images/photo'+znach+'.jpg';
-	bt2.src=v;
-	av.push(used);used=znach;
-
+function timeout(v){
+	bt2.src=v;$("#bt2").show();
 }
 
 function my_function(){
 	$("#bt2").fadeToggle({duration:2000});
-	setTimeout(function(){timeout()},2000);
-	$("#bt2").fadeToggle({duration:2000});
+	let v=random(0,av.length-1);
+	let znach=av[v];
+	av.splice(v,1);
+	v='images/photo'+znach+'.jpg';
+	document.body.style.backgroundImage='url(\''+v+'\''+')';av.push(used);used=znach;
+	setTimeout(function(){timeout(v)},2000);
+
 }
 setInterval(function(){my_function()},5000);
+
+
+
+
+
+
+
 
 document.getElementById('for_leisure').style.fontSize = $(window).height() * 0.05+'px';
 document.getElementById('free').style.fontSize = $(window).height() * 0.05+'px';

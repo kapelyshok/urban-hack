1bbbe5248ec6c56d329a2d9fac7d7aa44d6e82fc
julilishukk <qweasdqwe941@gmail.com>
2019-10-11 12:20:46 +0300

    end

--- js/index_1.js
+++ js/index_1.js
@@ -5,7 +5,7 @@ let registration_button=document.getElementById('registration_button');
 let bt_reg=document.getElementById('registration');
 
 //registration.onclick=function(){
-// 	$('#registration_button').on('click', function() { window.location = 'registration.html'; });
+	$('#registration_button').on('click', function() { window.location = 'registration.html'; });
 //}
 
 
@@ -16,27 +16,8 @@ function random(min, max) {
  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function timeout(v){
-	bt2.src=v;
-	$("#bt2").fadeToggle({duration:10});
-	console.log('zaraz')
-}
-
-function my_function(){
-	$("#bt2").fadeToggle({duration:2000});
-	let v=random(0,av.length-1);
-	let znach=av[v];
-	av.splice(v,1);
-	v='images/fon'+znach+'.png';
-	podkladka.src=v;
-	av.push(used);used=znach;
-	setTimeout(function(){timeout(v)},2000);
 
 
-}
-
-
-setInterval(function(){my_function()},6000);
 
 
 
@@ -52,6 +33,4 @@ window.onresize = function() {
 	//document.getElementById('enter').style.fontSize = $(window).height() * 0.05+'px';
 	//document.getElementById('registr').style.fontSize = $(window).height() * 0.05+'px';
 }
-
-
 

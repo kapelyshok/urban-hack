352097064f164ba2aed317bef98f5c7385853167
aleks <alekseevmr@ukr.net>
2019-10-11 08:46:54 +0300

    я геній

---
+++ js/list_gurt.js
@@ -0,0 +1,9 @@
+$('#volle').on('click', function() { window.location = 'main_gur.html'; });
+$('#logotip').on('click', function() { window.location = 'index.html'; });
+
+$('.lab,#box').on('mouseover', function () {$('.lab').css('textDecoration','underline')});
+$('.lab,#box').on('mouseout', function () {$('.lab').css('textDecoration','none')});
+$('.lab1,#volle').on('mouseover', function () {$('.lab1').css('textDecoration','underline')});
+$('.lab1,#volle').on('mouseout', function () {$('.lab1').css('textDecoration','none')});
+$('.lab2,#bascket').on('mouseover', function () {$('.lab2').css('textDecoration','underline')});
+$('.lab2,#bascket').on('mouseout', function () {$('.lab2').css('textDecoration','none')});
